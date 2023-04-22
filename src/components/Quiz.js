feb59ea957b1d551fd50bcae10b3000e47182377
Reactwithdev <avwithcoral@gmail.com>
2023-04-22 16:15:51 +0530

    format document

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -7,7 +7,7 @@ const Quiz = () => {
   const [currentQuestion, setCurrentQuestion] = useState(0)
   const [score, setScore] = useState(0)
   const [selectQuestion, setSelectQuestion] = useState(0)
-  const [showScore,setShowScore]=useState(false)
+  const [showScore, setShowScore] = useState(false)
 
 
   const changeQuestion = () => {
@@ -27,7 +27,7 @@ const Quiz = () => {
     console.log("scoreeeeeeeeeeeeeee", score)
   }
 
-  const ResetAll=()=>{
+  const ResetAll = () => {
 
     setShowScore(false);
     setCurrentQuestion(0);
@@ -38,33 +38,33 @@ const Quiz = () => {
     <>
       <p className='heading-txt'>QUIZ_APP</p>
       <div className='container'>
-        {showScore?(<TotelResult score={score} totelScore={QuizData.length} tryAgain={ResetAll}/>):(
+        {showScore ? (<TotelResult score={score} totelScore={QuizData.length} tryAgain={ResetAll} />) : (
           <>
-         
-        <div className='question'>
-          <span id="question-number">{currentQuestion + 1}</span>
-          <span id="question-number">{QuizData[currentQuestion].question}</span>
-        </div>
-        <div className='option-container'>
-          {QuizData[currentQuestion].options.map((option, index) => {
-            return (
-              <button
-                className={`option-btn ${selectQuestion == index + 1 ? "checked" : ""}`}
-                onClick={() => setSelectQuestion(index + 1)}
-                key={index}
-              >
-                {option}
-              </button>
-            )
-          })}
-        </div>
-        <input
-          type="button"
-          value="Next"
-          id="next-button"
-          onClick={changeQuestion}
-        />
- </>)}
+
+            <div className='question'>
+              <span id="question-number">{currentQuestion + 1}</span>
+              <span id="question-number">{QuizData[currentQuestion].question}</span>
+            </div>
+            <div className='option-container'>
+              {QuizData[currentQuestion].options.map((option, index) => {
+                return (
+                  <button
+                    className={`option-btn ${selectQuestion == index + 1 ? "checked" : ""}`}
+                    onClick={() => setSelectQuestion(index + 1)}
+                    key={index}
+                  >
+                    {option}
+                  </button>
+                )
+              })}
+            </div>
+            <input
+              type="button"
+              value="Next"
+              id="next-button"
+              onClick={changeQuestion}
+            />
+          </>)}
       </div>
     </>
   )
